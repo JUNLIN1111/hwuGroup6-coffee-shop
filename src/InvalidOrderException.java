5fac7f159f76e3f6831687de2a04533d44cc02a0
Wzyoung203 <2015201665@qq.com>
2025-03-04 23:43:09 +0000

    Update InvalidOrderException.java

--- src/InvalidOrderException.java
+++ src/InvalidOrderException.java
@@ -3,4 +3,26 @@ public class InvalidOrderException extends Exception {
 	public InvalidOrderException(){
 		super("This Order is invalid.");
 	}
+	
+	public InvalidOrderException(String message){
+		super(message);
+	}
+}
+
+class InvalidOrderIdException extends InvalidOrderException{
+	public InvalidOrderIdException(String message){
+		super(message);
+	}
+}
+
+class InvalidOrderTimeStampException extends InvalidOrderException{
+	public InvalidOrderTimeStampException(String message){
+		super(message);
+	}
+}
+
+class IllegalitemListException extends InvalidOrderException{
+	public IllegalitemListException(String message){
+		super(message);
+	}
 }
