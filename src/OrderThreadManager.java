532e4b225c62226665ca37bb33faad2f78a7eed6
JUNLIN1111 <Wu1549983212@163.com>
2025-04-03 20:41:03 +0100

    pass parameter for thread
    pass parameter for thread

--- src/OrderThreadManager.java
+++ src/OrderThreadManager.java
@@ -3,13 +3,15 @@ import java.util.List;
 
 public class OrderThreadManager {
     private final List<Thread> serverThreads = new ArrayList<>();
+    private final ThreadedOrderProcessor processor;
 
-    public OrderThreadManager() {
+    public OrderThreadManager(ThreadedOrderProcessor processor) {
+        this.processor = processor;
     }
 
     public void startServers(int count) {
         for (int i = 1; i <= count; i++) {
-            ServerThread task = new ServerThread("Server-" + i);
+            ServerThread task = new ServerThread("Server-" + i, processor);
             Thread thread = new Thread(task);
             thread.start();
             serverThreads.add(thread);
@@ -21,6 +23,4 @@ public class OrderThreadManager {
             thread.interrupt();
         }
     }
-
-
 }
